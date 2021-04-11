c38885adc9bf75d8eb46cbb3bf1811595a38d274
cocodori <moradulee@gmail.com>
2021-04-11 14:37:03 +0900

    create project!

---
+++ src/main/java/com/newscrawler/NewsCrawlerApplication.java
@@ -0,0 +1,13 @@
+package com.newscrawler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsCrawlerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewsCrawlerApplication.class, args);
+    }
+
+}

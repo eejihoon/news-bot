0ac4c39e3a6e52efe5e7bcb219e87893fdbf47dc
cocodori <moradulee@gmail.com>
2021-04-17 14:17:14 +0900

    add: 매일 6시에 메일 발송하는 스케쥴러 생성

---
+++ src/main/java/com/newscrawler/newsletter/SendNewsletterJob.java
@@ -0,0 +1,25 @@
+package com.newscrawler.newsletter;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class SendNewsletterJob {
+    private final EmailService emailService;
+    private final NewsLetterService newsLetterService;
+
+    @Scheduled(cron = "0 0 6 * * *")
+    public void sendNewsletter() {
+        log.info("==========================메일 발송을 시작합니다.===============================");
+        emailService.getEmails()
+                .forEach(emailAddress -> {
+                    newsLetterService.createNewsArticles(emailAddress.getEmail());
+                    log.info(emailAddress +"주소로 이메일을 발송했습니다.");
+                });
+        log.info("========================모든 구독자에게 메일 발송을 완료했습니다.===================");
+    }
+}

db90b76111ad4d984097bab5d6b834156846a36c
cocodori <moradulee@gmail.com>
2021-04-17 15:11:56 +0900

    add: EmailAddress엔티티 등록, Repository생성

---
+++ src/main/java/com/newscrawler/newsletter/repository/EmailAddressRepository.java
@@ -0,0 +1,8 @@
+package com.newscrawler.newsletter.repository;
+
+import com.newscrawler.newsletter.domain.EmailAddress;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+public interface EmailAddressRepository extends JpaRepository<EmailAddress, Long> {
+}

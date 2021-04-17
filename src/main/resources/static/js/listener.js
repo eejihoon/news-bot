4cd444e8de3a7842bdb93429819b9f21e28ecf58
cocodori <moradulee@gmail.com>
2021-04-17 17:09:53 +0900

    add: 구독취소 화면처리

--- src/main/resources/static/js/listener.js
+++ src/main/resources/static/js/listener.js
@@ -1,25 +1,52 @@
 const listener = {
-    init: () => {
+    init: function () {
+        const _this = this;
         document.querySelector("#btn-subscribe").addEventListener("click", (e) => {
                 e.preventDefault();
-                const email = document.querySelector("#emailAddress").value;
-                const xmlHttpRequest = new XMLHttpRequest();
-                xmlHttpRequest.open('POST', '/api/subscribe');
-                xmlHttpRequest.setRequestHeader("Content-Type", "application/json");
-                xmlHttpRequest.send(JSON.stringify(email));
-
-                xmlHttpRequest.onload = function () {
-                    if (xmlHttpRequest.status === 200 || xmlHttpRequest === 201) {
-                        alert("이메일이 등록되었습니다.");
-                        window.location.href = "/subscribe";
-                    } else if (xmlHttpRequest.status === 400) {
-                        alert("잘못된 이메일입니다.");
-                    } else if (xmlHttpRequest.status === 409) {
-                        alert('이미 등록된 이메일입니다.');
-                    }
-                }
+                _this.subscribe();
             }
         )
+
+        document.querySelector("#btn-cancel-subscribe").addEventListener("click", (e) => {
+            e.preventDefault();
+            _this.cancelSubscribe();
+        })
+    },
+
+    subscribe: function () {
+        const email = document.querySelector("#emailAddress").value;
+        const xmlHttpRequest = new XMLHttpRequest();
+        xmlHttpRequest.open('POST', '/api/subscribe');
+        xmlHttpRequest.setRequestHeader("Content-Type", "application/json");
+        xmlHttpRequest.send(JSON.stringify(email));
+
+        xmlHttpRequest.onload = function () {
+            if (xmlHttpRequest.status === 200 || xmlHttpRequest === 201) {
+                alert("이메일이 등록되었습니다.");
+                window.location.href = "/subscribe";
+            } else if (xmlHttpRequest.status === 400) {
+                alert("잘못된 이메일입니다.");
+            } else if (xmlHttpRequest.status === 409) {
+                alert('이미 등록된 이메일입니다.');
+            }
+        }
+    },
+    cancelSubscribe : function () {
+        const email = document.querySelector("#cancelEmail");
+        const xmlHttpRequest = new XMLHttpRequest();
+        xmlHttpRequest.open('DELETE', '/api/subscribe');
+        xmlHttpRequest.setRequestHeader("Content-Type", "application/json");
+        xmlHttpRequest.send(JSON.stringify(email.value));
+
+        xmlHttpRequest.onload = function () {
+            if (xmlHttpRequest.status === 200) {
+                alert('구독 취소되었습니다.');
+                window.location.href = '/subscribe';
+            } else {
+                alert('잘못된 이메일입니다.');
+            }
+        }
+
     }
 }
 listener.init();

c728e84eff8a5094287efcb67ea1b188bf726187
cocodori <moradulee@gmail.com>
2021-04-18 21:21:17 +0900

    add: view test controller 추가

--- src/main/java/com/newscrawler/newsletter/web/SubscribeNewsletterController.java
+++ src/main/java/com/newscrawler/newsletter/web/SubscribeNewsletterController.java
@@ -1,11 +1,40 @@
 package com.newscrawler.newsletter.web;
 
+import com.newscrawler.crawl.*;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 public class SubscribeNewsletterController {
     @GetMapping("/subscribe")
     public void subscribe() {
+    }
+    //TODO Test Controller 추후 삭제
+    @GetMapping("news")
+    public String test(Model model) {
+        Article article = new Article();
+        List<Crawler> crawlers = Arrays.asList(
+                new HaniCrawl(),
+                new KhanCrawl(),
+                new DongaCrawl(),
+                new DongaCrawl(),
+                new JoongangCrawl(),
+                new ScienceTimesCrawl());
+
+        List<Article> articles = new ArrayList<>();
+
+        crawlers.forEach(crawler -> {
+            List<Article> articles1 = article.getArticles(crawler);
+            articles1.forEach(a -> articles.add(a));
+        });
+
+        model.addAttribute("articles", articles);
+
+        return "mail/news";
     }
 }
